23eee2c8ee09099bdcf0c80e744f238d667c84b7
Adi Sai <adithya.sairam1@gmail.com>
2014-12-11 15:02:41 -0500

    Add new classes

---
+++ 2D Arrays/Arrays2D_Ex2.java
@@ -0,0 +1,55 @@
+import java.util.*;
+public class Arrays2D_Ex2
+{
+    static int ARR_SIZE = 10;
+    static int MAX_VALUE = 100;
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Favorite Number: ");
+        int favnum = scan.nextInt();
+        int[][] arr2D = new int[ARR_SIZE][ARR_SIZE];
+        
+        for (int row=0; row<arr2D.length; row++)
+        {
+            for (int col=0; col<arr2D[row].length; col++)
+            {
+                arr2D[row][col] = (int)(Math.random()*MAX_VALUE+1);
+            }
+        }
+        
+        printArr(arr2D);  // print the before
+        
+        System.out.println();
+        
+        /*# INSERT YOUR BEAUTIFUL CODE HERE   */
+        int row = 0;
+        int col = (arr2D[row].length) - 1;
+        for (int r=0; r<arr2D.length; r++)
+        {
+            arr2D[col][row] = favnum;
+            row++;
+            col--;
+        }
+        
+        printArr(arr2D);  // print the after
+        
+      
+    }//end main
+       
+    public static void printArr(int[][] arrToPrint)
+    {
+        for (int r=0; r<arrToPrint.length; r++)
+        {
+            for (int c=0; c<arrToPrint[r].length; c++)
+            {
+                if (arrToPrint[r][c] < 10)
+                    System.out.print(" ");
+                if (arrToPrint[r][c] < 100)
+                    System.out.print(" ");
+                System.out.print(arrToPrint[r][c]+"  ");
+            } // end of row
+            System.out.println(); //change lines
+        }
+    }
+}
---
+++ Misc/RandomNumberGenerator.java
@@ -0,0 +1,42 @@
+
+/**
+ * Write a description of class RandomNumber here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.*;
+public class RandomNumberGenerator
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Max Value: ");
+        int MAX_VALUE = (int)scan.nextDouble();
+        if (MAX_VALUE > Integer.MAX_VALUE)
+        {
+            MAX_VALUE = Integer.MAX_VALUE;
+        }
+        int x = (int)(Math.random()*MAX_VALUE+1);
+        int count = 0;
+        if (x > 0 && x < 101)
+        {
+            count++;
+            System.out.println("At Count: " + count + ", Random Number: " + x);
+        }
+        else
+        {
+            for (int i = 0; i < Integer.MAX_VALUE; i++)
+            {
+                count++;
+                x = (int)(Math.random()*MAX_VALUE+1);
+                System.out.println("At Count: " + count + ", Random Number: " + x);
+                if (x > 0 && x < 101)
+                {
+                    System.out.println("At Count: " + count + ", Random Number: " + x);
+                    break;
+                }
+            }
+        }
+     }
+}
