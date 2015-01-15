48efc4920c400001a3450fdaf27ee83525a879dc
Adi Sai <adithya.sairam1@gmail.com>
2015-01-14 23:51:30 -0500

    Update ExtraCredit.java

--- Array Lists/BigBucks/ExtraCredit.java
+++ Array Lists/BigBucks/ExtraCredit.java
@@ -1,6 +1,5 @@
-
 /**
- * Write a description of class ExtraCredit  here.
+ * Write a description of class ExtraCredit here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
@@ -12,57 +11,54 @@ public class ExtraCredit
     {
         Scanner scan  = new Scanner(System.in);
         String st = "";
-        double val = 0;
+        double val = 0; double change = 0;
+        int choice;
         ArrayList<BankAccount> accts = new ArrayList<BankAccount>();
-        System.out.println("Please choose an option from below:");
-        System.out.println("1: Deposit");
-        System.out.println("2: Withdraw");
-        System.out.println("3: Print Max");
-        System.out.println("4: Exit");
-        System.out.println();
-        int choice = scan.nextInt();
-        if (choice == 1 || choice == 2 || choice == 3 || choice == 4)
+        System.out.println("Please enter a name to whom the account belongs.");
+        st = scan.nextLine();
+        System.out.println("Please input the initial value.");
+        val = scan.nextDouble();
+        BankAccount ba = new BankAccount(st, val);
+        accts.add(ba);
+        while (true)
         {
-           if (choice == 1)
-           {
-               
-           }
-           if (choice == 2)
-           {
-               
-           }
-           if (choice == 3)
-           {
-               printLargest(accts);
-           }  
-           if (choice == 4)
-           {
-              System.exit(0);
-           }
-        } 
-        else if (choice != 1 || choice != 2 || choice != 3 || choice != 4)
-        {
-            System.out.println(choice + " is not a valid option.");
-            System.err.println(choice + " is out of bounds.");
+            System.out.println("\n" + "Please choose an option from below:");
+            System.out.println("1: Deposit");
+            System.out.println("2: Withdraw");
+            System.out.println("3: Print Max");
+            System.out.println("4: Exit");
+            choice = scan.nextInt();
+            if (choice == 1 || choice == 2 || choice == 3 || choice == 4)
+            {
+                if (choice == 1)
+                {
+                    System.out.println("Please enter the amount of the deposit:");
+                    change = scan.nextDouble();
+                    ba.deposit(change);
+                }
+                if (choice == 2)
+                {
+                    System.out.println("Please enter the amount of the withdrawl:");
+                    change = scan.nextDouble();
+                    ba.withdraw(change);
+                }
+                if (choice == 3)
+                {
+                    printLargest(accts);
+                }  
+                if (choice == 4)
+                {
+                    break;
+                }
+            } 
+            else if (choice != 1 || choice != 2 || choice != 3 || choice != 4)
+            {
+                System.out.println(choice + " is not a valid option.");
+            }
         }
-//         for (int i = 0; i < Integer.MAX_VALUE; i++)
-//         {
-//             System.out.println("Please enter a name to whom the account belongs. (\"Exit\" to abort)");
-//             st = scan.nextLine();
-//             if (st.equals("Exit"))
-//             {
-//                 break;
-//             }
-//             System.out.println("Please enter the amount of the deposit");
-//             val = scan.nextDouble();
-//             System.out.println();
-//             scan.nextLine();
-//             BankAccount ba = new BankAccount(st, val);
-//             accts.add(ba);
-//         }
-//         printLargest(accts);
-     }
-    
+        System.exit(0);
+    }
+
     public static void printLargest(ArrayList<BankAccount> acctsList)
     {
         double max = 0;
